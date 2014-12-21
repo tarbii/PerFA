d5648736bb64eb5acfe2ee329d2857ff4d0f4c1e
Taras Babii <taras.babii@gmail.com>
2014-12-21 11:41:27 +0200

    cleaned DetailedTransaction

--- PerFA/Model/DetailedTransaction.cs
+++ PerFA/Model/DetailedTransaction.cs
@@ -6,148 +6,40 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using PerFA.Annotations;
-using PerFA.Model.TransactionTypes;
+using PropertyChanged;
 
 namespace PerFA.Model
 {
-    class DetailedTransaction : INotifyPropertyChanged
+    [ImplementPropertyChanged]
+    class DetailedTransaction
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        //private Dictionary<string, decimal?> usersSumsDictionary;
-
         private DatabaseContext db;
         private readonly TransactionUser transactionUser;
+        public TransactionUser TransactionUser { get { return transactionUser; } }
 
         public DetailedTransaction(TransactionUser transactionUser, DatabaseContext db)
         {
             this.transactionUser = transactionUser;
             this.db = db;
-            MultiuserManager = new MultiuserManager(
+            multiuserManager = new MultiuserManager(
                 db, transactionUser.ID_user, transactionUser.ID_transaction);
             if (transactionUser.Transaction.HouseholdExpence != null)
             {
-                householdExpensesDetails = 
-                    new DTHouseholdExpenses(transactionUser.Transaction.HouseholdExpence);
                 type = "Побутові витрати";
             }
             else if (transactionUser.Transaction.Wage != null)
             {
-                wageDetails = new DTWage(transactionUser.Transaction.Wage);
                 type = "Заробітня плата";
             }
         }
         
-        public DateTime? Date
-        {
-            get { return transactionUser.Transaction.Date; }
-            set
-            {
-                if (transactionUser.Transaction.Date != value)
-                {
-                    transactionUser.Transaction.Date = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public string Description
-        {
-            get { return transactionUser.Transaction.Description; }
-            set
-            {
-                if (transactionUser.Transaction.Description != value)
-                {
-                    transactionUser.Transaction.Description = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public string UserName
-        {
-            get { return transactionUser.User.Name; }
-        }
-
-        public decimal? Sum
-        {
-            get { return transactionUser.Sum; }
-            set
-            {
-                if (transactionUser.Sum != value)
-                {
-                    transactionUser.Sum = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public string AuthorName
-        {
-            get { return transactionUser.Transaction.User.Name; }
-        }
-
-        //public ICollection<TransactionUser> UsersSumsDictionary
-        //{
-        //    get { return transactionUser.Transaction.TransactionUsers; }
-        //    set
-        //    {
-        //        if (transactionUser.Transaction.TransactionUsers != value)
-        //        {
-        //            transactionUser.Transaction.TransactionUsers = value;
-        //            OnPropertyChanged();
-        //        }
-        //    }
-        //}
-
-        private MultiuserManager multiuserManager;
-        public MultiuserManager MultiuserManager
-        {
-            get { return multiuserManager; }
-            set
-            {
-                if (multiuserManager != value)
-                {
-                    multiuserManager = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+        private readonly MultiuserManager multiuserManager;
+        public MultiuserManager MultiuserManager { get { return multiuserManager; } }
 
         private readonly string type;
         public string Type
         {
             get { return type; }
         }
-
-        private readonly DTHouseholdExpenses householdExpensesDetails;
-        public DTHouseholdExpenses HouseholdExpensesDetails
-        {
-            get { return householdExpensesDetails; }
-        }
-
-        public bool HasHouseholdExpensesDetails
-        {
-            get { return householdExpensesDetails != null; }
-        }
-
-        private readonly DTWage wageDetails;
-        public DTWage WageDetails
-        {
-            get { return wageDetails; }
-        }
-
-        public bool HasWageDetails
-        {
-            get { return wageDetails != null; }
-        }
-
     }
 }
--- PerFA/Model/TransactionClass.cs
+++ PerFA/Model/TransactionClass.cs
@@ -8,7 +8,6 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using PerFA.Annotations;
-using PerFA.Model.TransactionTypes;
 
 namespace PerFA.Model
 {
